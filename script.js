e941e4e30703935f88cea345ec944be8d4d59d1f
MUNTHA PAVAN KUMAR <muntha.pavan@gmail.com>
2021-01-07 14:40:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 let countries = [];
+let url = 'https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json';
 
-let p =  new Promise((resolve, reject)=>{
-    let request = new XMLHttpRequest();
-    request.open('GET', 'https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json', true);
-    request.onerror = function(){
-        console.log('Error');
-        reject('An error occured');
-    }
-    request.send();
-    request.onload = function () {
-       let data = JSON.parse(this.response);
-       resolve(data)
-    }
-})
-p.then((data)=>{
+
+
+fetch(url).then((data)=>{
+    return data.json(); 
+}).then((data)=>{
     countries = getCountries(data); 
     createRows(countries);
 }).catch((error)=>{
-    console.log(error)
+    console.log(error);
+    let err = createElement('p', 'display-4 text-center')
+    err.setAttribute('style', 'color: red')
+    err.innerText = 'Oops! we encountered an error'
+    document.getElementById('countrycards').append(err)
 })
 
 let createElement = (element, className='', id='')=>{
@@ -28,10 +24,33 @@ let createElement = (element, className='', id='')=>{
 }
 
 let createli = (key, innerhtml) => {
-    let li = createElement('li', 'list-group-item');
+    let li = createElement('li', 'list-group-item text-center');
+    if(key === 'button'){
+        let button = createElement('button', 'btn btn-light');
+        button.onclick = ()=>{
+            getWeather(innerhtml)
+        }
+        button.innerText = 'Get Weather'
+        li.append(button);
+        return li;
+    }
     li.innerHTML = key+' : '+innerhtml;
     return li;
 
+}
+
+function getWeather(capital){
+    console.log(capital)
+    let api_key = '8b64dd88e9c8ef3360842ef7f6dc1a2d';
+    let weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+capital+'&appid='+api_key
+    fetch(weatherUrl).then((data)=>{
+        return data.json();
+    }).then((data)=>{
+        alert(capital+' current weather : '+data.weather[0].description);
+    }).catch((error)=>{
+        alert('Weather not available for this Country');
+    })
+    
 }
 
 let createCard = (obj) =>{
@@ -51,8 +70,9 @@ let createCard = (obj) =>{
    let region = createli('Region', obj.region);
    let lat = (+obj.latlng[0]).toFixed(2)
    let lng = (+obj.latlng[1]).toFixed(2)
-   let latLong = createli('Lat-Long', lat+', '+lng)
-   ul.append(capital, codes, region, latLong);
+   let latLong = createli('Lat-Long', lat+', '+lng);
+   let buttonli = createli('button', obj.capital)
+   ul.append(capital, codes, region, latLong, buttonli);
    card.append(cardHeader, img, ul);
    column.append(card);
    return column;
@@ -84,6 +104,8 @@ let getCountries =  (obj)=>{
     }
     return countries1;
 }
+
+
 
 
 
